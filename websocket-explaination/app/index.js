3df98f2a8419b44d5b9b8bf911e84106a6c5aa5a
NuroC <america.alex.arn@icloud.com>
2023-03-20 20:43:16 -0400

    Inital Commit

---
+++ websocket-explaination/app/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const expressWs = require('express-ws');
+const path = require('path');
+
+const app = express();
+
+const encode = require("./src/encode");
+const decode = require("./src/decode");
+
+
+// initialize express-ws into express
+expressWs(app);
+
+// Serve HTML file on the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/main.html'));
+});
+
+
+// Create WebSocket endpoint
+app.ws('/ws', (ws, req) => {
+    ws.send(JSON.stringify({
+        type: "welcome",
+        data: encode("Hello, World!", 4)
+    }))
+    ws.on('message', (msg) => {
+        let decoded = decode(msg, 5)
+        // random number between 1 and 10
+        let shift = Math.round(Math.random() * 10) + 1;
+
+        ws.send(JSON.stringify({
+            type: "response",
+            data: encode(decoded, shift),
+            shift: shift
+        }));
+    });
+});
+
+// Start server on PORT 3000
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
---
+++ websocket-explaination/app/src/decode.js
@@ -0,0 +1,34 @@
+
+// shift to the left
+function decode(inputString, numChars) {
+    // Define a string with the alphabet
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    // Convert the input string to lowercase
+    inputString = inputString.toLowerCase();
+    // Initialize an empty string to hold the shifted string
+    let shiftedString = "";
+
+    // Loop through each character in the input string
+    for (let i = 0; i < inputString.length; i++) {
+        // Check if the current character is a letter
+        if (alphabet.includes(inputString[i])) {
+            // Find the index of the current character in the alphabet string
+            let charIndex = alphabet.indexOf(inputString[i]);
+            // Calculate the new index of the shifted character
+            let shiftedIndex = (charIndex - numChars) % 26;
+            if (shiftedIndex < 0) {
+                shiftedIndex += 26;
+            }
+            // Add the shifted character to the shifted string
+            shiftedString += alphabet[shiftedIndex];
+        } else {
+            // If the current character is not a letter, add it to the shifted string as-is
+            shiftedString += inputString[i];
+        }
+    }
+
+    // Return the shifted string
+    return shiftedString;
+}
+
+module.exports = decode
